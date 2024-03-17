9f2d693723db78a60beec158bd28167482149ffc
clookin <sebast305@hotmail.com>
2024-03-17 13:24:38 -0500

    MediaQ phone

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const nav = document.querySelector('.nav');
 const copy = document.querySelector('.copy');
 const textCenter = document.querySelector('.text-center');
 const labels = document.querySelectorAll('.label');
+const headerList = document.querySelector('.header-list');
+const headerLista = document.querySelectorAll('.link-ab');
+
 
 
 btnSwitch.addEventListener('click',()=>{
@@ -15,6 +18,7 @@ btnSwitch.addEventListener('click',()=>{
   mainContainer.classList.toggle('dark');
   copy.classList.toggle('dark');
   textCenter.classList.toggle('dark');
+  headerList.classList.toggle('dark')
   labels.forEach(label => {
     label.classList.toggle('dark');
 });
@@ -22,7 +26,14 @@ btnSwitch.addEventListener('click',()=>{
 
 menuBtn.addEventListener('click', ()=>{
   nav.classList.toggle('active');
+  menuBtn.classList.toggle('menu-button-gr')
 })
+headerLista.forEach(link => {
+  link.addEventListener('click', ()=>{
+      nav.classList.remove('active')
+      menuBtn.classList.remove('menu-button-gr')
+  })
+});
 
 ScrollReveal().reveal('.container_input', { delay: 400 });
 ScrollReveal().reveal('.form', { delay: 400 });
@@ -30,3 +41,4 @@ ScrollReveal().reveal('.projects', { delay: 400 });
 ScrollReveal().reveal('.main__section-tecno', { delay: 400 });
 ScrollReveal().reveal('.main__section-aboutMe', { delay: 400 });
 ScrollReveal().reveal('.main-container', { delay: 400 });
+
